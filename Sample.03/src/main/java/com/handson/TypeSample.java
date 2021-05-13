3515e2f0e045341f32b53fc82b044a1927d8c5c0
Shuto Horie <shuto.horie.sb@gmail.com>
2021-05-13 11:25:35 +0900

    add Sample.03 handson

--- Sample.03/src/main/java/com/handson/TypeSample.java
+++ Sample.03/src/main/java/com/handson/TypeSample.java
@@ -3,19 +3,19 @@ package com.handson;
 class TypeSample {
     public static void main(String[] args) {
         // TODO 整数
-
-
+        int sum = 5;
+        System.out.println(sum);
 
         // TODO 浮動小数点数
-
-
+        double rate = 10.5;
+        System.out.println(rate);
 
         // TODO 真偽値
-
-
+        boolean isHoliday = true;
+        System.out.println(isHoliday);
 
         // TODO 文字列
-
-
+        String title = "Java基礎";
+        System.out.println(title);
     }
 }
--- Sample.03/src/main/java/com/handson/VariableSample.java
+++ Sample.03/src/main/java/com/handson/VariableSample.java
@@ -1,29 +1,33 @@
 package com.handson;
 
+
+
 class VariableSample {
     public static void main(String[] args) {
         // TODO int型の変数countを宣言する
-
+        int count;
 
         // TODO countの中身に1を代入する
-
+        count = 1;
 
         // TODO 変数countの中身をコンソールに出力する
-
+        System.out.println(count);
 
         // TODO countという文字列をコンソールに出力する
-
+        System.out.println("count");
 
         // TODO 変数countの中身を2に書き換える
-
-
+        count = 2;
+        System.out.println(count);
 
         // TODO 別の変数ageを宣言し、宣言と同時に3を代入する
-
-
+        int age = 3;
+        System.out.println(age);
 
         // TODO ageの中身をcountの中身で上書きする
-
+        age = count;
+        System.out.println(age);
+        System.out.println(count);
 
 
     }
