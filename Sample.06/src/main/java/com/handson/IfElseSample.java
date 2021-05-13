0770352054622a743759b372c10a8f7fbb89d4f0
Shuto Horie <shuto.horie.sb@gmail.com>
2021-05-13 14:14:18 +0900

    add Sample.06 handson

--- Sample.06/src/main/java/com/handson/IfElseSample.java
+++ Sample.06/src/main/java/com/handson/IfElseSample.java
@@ -15,6 +15,22 @@ class IfElseSample {
         // 30以上：今日は真夏日です。
         // 25以上：今日は夏日です。
         // その他：今日は猛暑日・真夏日・夏日ではない日です。
+        if (maxTemperature >= 35) {
+            System.out.println("今日は猛暑日です。");
+        } else {
+            System.out.println("今日は猛暑日ではありません。");
+        }
+        if (maxTemperature >= 30) {
+            System.out.println("今日は真夏日です。");
+        }
+        else {
+            System.out.println("今日は真夏日ではありません。");
+        }
+        if (maxTemperature >= 25) {
+            System.out.println("今日は夏日です。");
+        } else {
+            System.out.println("今日は夏日ではありません。");
+        }
 
 
 
--- Sample.06/src/main/java/com/handson/LoopSample.java
+++ Sample.06/src/main/java/com/handson/LoopSample.java
@@ -8,30 +8,30 @@ class LoopSample {
         List<Integer> scoreList =  List.of(74, 71, 92, 68, 58);
 
         // TODO 単純な繰り返し処理
-
-
-
+        for (int score: scoreList) {
+            System.out.println(score);
+        }
 
         // TODO ブロックには複数の命令を記述できる
-
-
-
-
-
-
+        int sum = 0;
+        for (int score : scoreList) {
+            sum = sum + score;
+            System.out.println("score: " + score);
+        }
+        System.out.println("合計点：" + sum);
 
         // TODO 処理をネストする
         // 受けていない試験の得点を−1で示す
         List<Integer> scores = List.of(74, 71, 92, -1, 58);
-
-
-
-
-
-
-
-
-
+        int sum2 = 0;
+        for (int score: scores) {
+            if (score >= 0) {
+                sum2 = sum2 + score;
+            } else {
+                System.out.println("受験していないため加算しませんでした。");
+            }
+        }
+        System.out.println("合計店：" + sum2);
 
     }
 }
