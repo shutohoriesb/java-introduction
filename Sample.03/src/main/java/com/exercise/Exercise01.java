19945860cd79fdcd03e8ee8aad950c6aee35c83d
Shuto Horie <shuto.horie.sb@gmail.com>
2021-05-13 13:30:33 +0900

    add Sample.03 Exercise

--- Sample.03/src/main/java/com/exercise/Exercise01.java
+++ Sample.03/src/main/java/com/exercise/Exercise01.java
@@ -4,12 +4,14 @@ class Exercise01 {
     public static void main(String[] args) {
 
         // TODO int型の変数numを宣言し、10を代入する
-
+        int num = 10;
 
         // TODO String型の変数messageを宣言し、"Hello"を代入する
-
+        String message = "Hello";
 
         // TODO それぞれの変数の中身をコンソールに出力する
+        System.out.println(num);
+        System.out.println(message);
 
 
     }
--- Sample.03/src/main/java/com/exercise/Exercise02.java
+++ Sample.03/src/main/java/com/exercise/Exercise02.java
@@ -4,10 +4,12 @@ class Exercise02 {
     public static void main(String[] args) {
         // TODO 変数を2つ用意し、それぞれの変数に自分の名前、年齢を代入する
         // 型と変数名は自分で判断する
-
-
+        int age = 25;
+        String name = "Horie";
 
         // TODO それぞれの変数の中身をコンソールに出力する
+        System.out.println(age);
+        System.out.println(name);
 
 
     }
