2715a2a647b98c8242979e60df08538d1c90c4cc
Shuto Horie <shuto.horie.sb@gmail.com>
2021-05-13 14:22:05 +0900

    add Sample.04 exercise

--- Sample.04/src/main/java/com/exercise/Exercise01.java
+++ Sample.04/src/main/java/com/exercise/Exercise01.java
@@ -4,29 +4,28 @@ class Exercise01 {
     public static void main(String[] args) {
 
         // TODO 4+9 の演算をするプログラムを記述し、結果をコンソールに出力する
-
-
+        int value1  = 4+9;
+        System.out.println(value1);
 
         // TODO 3×6÷2 の演算をするプログラムを記述し、結果をコンソールに出力する
-
-
+        int value2  = 3*6/2;
+        System.out.println(value2);
 
         // TODO 4,9,5,6,8の平均値(6.4)を求める演算を記述し、結果をコンソールに出力する
-
-
-
-
+        double value3  = (double)(4+9+5+6+8)/5;
+        System.out.println(value3);
 
         // TODO 4と9を結合して49にする演算を記述し、結果をコンソールに出力する
-
-
+        String value4 = "4"+"9";
+        System.out.println(value4);
 
         // TODO 整数値の1と2が等しいかどうか確かめる演算を記述し、結果をコンソールに出力する
-
-
+        boolean value5 = 1==2;
+        System.out.println(value5);
 
         // TODO 文字列の"Apple"と"りんご"が等しいかどうか確かめる演算を記述し、結果をコンソールに出力する
-
+        boolean value6 = "Apple"=="りんご";
+        System.out.println(value6);
 
     }
 }
